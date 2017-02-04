c40c179c074d879e13c6c1fdc8d9df2187fa7c73
Amanda Georgescu <georgescu.amanda@gmail.com>
2017-02-04 17:01:20 -0500

    finish testing for current logic

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ class Test(TestCase):
 		db.session.remove()
 		db.drop_all()
 
-	# Util Tests
+	# Application Util Tests
 	def test_validate_user_not_exists(self):
 		exists = app.validate_user_exists("georgescu.amanda@gmail.com")
 		self.assertEqual(exists, False)
@@ -63,7 +63,7 @@ class Test(TestCase):
 		validation = app.validate_sender_and_receiver("bob@gmail.com", "georgescu.amanda@gmail.com")
 		self.assertEqual(validation, None)
 
-	#Route and Controller Tests
+	# Route and Controller Tests
 	def test_add_user_already_exists(self):
 		user = app.User("Bob","bob@gmail.com")
 		db.session.add(user)
@@ -72,11 +72,74 @@ class Test(TestCase):
 		self.assertEqual(json.loads(response.data).items(), {"status":"Error","message":"User with this email address already exists!"}.items())
 
 	def test_add_user_name_too_long(self):
-		user = app.User("Bobrjhfjrkhjrhjhrtjhrjthjrhtjrehtjhrjthrejhtjrehtjhrejtkhrejkhtjkrhtjherjkthjkerhtjkerhtjk","bob@gmail.com")
-		db.session.add(user)
-		db.session.commit()
-		response = self.client.post('/users', data='{"name":"Bob","email":"bob@gmail.com"}', content_type='application/json')
+		response = self.client.post('/users', data='{"name":"Bobhghghjghjgjhghjghgjhghjghjghjghjghjghjghjghjghjgjhghjgjhghjghjghjghjghjghjgjhgjhhjghjghjgjghghjgjhgh","email":"bob@gmail.com"}', content_type='application/json')
 		self.assertEqual(json.loads(response.data).items(), {"status":"Error","message":"Email and name cannot be more than 64 chars each!"}.items())
+
+	def test_add_user_successful(self):
+		response = self.client.post('/users', data='{"name":"Bob","email":"bob@gmail.com"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data).items(), {"status":"Success","message":"Congrats, Bob! You're now ready to chat!"}.items())
+
+	def test_query_all_users_not_empty(self):
+		user1 = app.User("Bob","bob@gmail.com")
+		db.session.add(user1)
+		user2 = app.User("Amanda Georgescu","georgescu.amanda@gmail.com")
+		db.session.add(user2)
+		db.session.commit()
+		response = self.client.get('/users')
+		self.assertEqual(json.loads(response.data), [{"name":"Bob","email":"bob@gmail.com"},{"name":"Amanda Georgescu","email":"georgescu.amanda@gmail.com"}])
+
+	def test_query_all_users_empty(self):
+		response = self.client.get('/users')
+		self.assertEqual(json.loads(response.data).items(), {"status":"Error","message":"There is nobody to chat with yet!"}.items())
+
+	def test_post_message_validation_error(self):
+		response = self.client.post('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com","text":"Hey Bob"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data).items(), json.loads(app.validate_sender_and_receiver("georgescu.amanda@gmail.com","bob@gmail.com")).items())
+
+	def test_post_message_too_long(self):
+		user1 = app.User("Bob","bob@gmail.com")
+		db.session.add(user1)
+		user2 = app.User("Amanda Georgescu","georgescu.amanda@gmail.com")
+		db.session.add(user2)
+		db.session.commit()
+		response = self.client.post('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com","text":"Hey Bob848748738497389748937djhfjdkhfjkdshfjkdhfjkhdjkfhjkdhfjkdhfjkhdjkhfjkdhfjkhdsjkfhjkdshfjkdhfjkhdsjhfhghghfdye5ruhfjhdfijdhsfksdjkfhdsjkfhjkdshfjkhdsjfkhsdkjhfjksdhfjkhdskjfhjksdhfjkhdsjkfhdsjkhfjksdhfjkhsdkjfhsdjkhfjkdshjkfhkjdshfjdshfjkhdsjkfhjkdshfjkhdsjkfhkjdshfjkdshfjkhdsjkhfkdshfjkdshfjkhdkjdhsfjkhdsjkfhjksdhfjdhsfjkhdsjkfhdskjhjkdshfjkhdskjfhjkdshfjkhsdjkfh"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data).items(), {"status":"Error","message":"Message is too long to send!"}.items())
+
+	def test_post_message_successful(self):
+		user1 = app.User("Bob","bob@gmail.com")
+		db.session.add(user1)
+		user2 = app.User("Amanda Georgescu","georgescu.amanda@gmail.com")
+		db.session.add(user2)
+		db.session.commit()
+		response = self.client.post('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com","text":"Hey Bob"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data).items(), {"name":"Amanda Georgescu","text":"Hey Bob"}.items())
+
+	def test_get_message_validation_error(self):
+		response = self.client.get('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data).items(), json.loads(app.validate_sender_and_receiver("georgescu.amanda@gmail.com","bob@gmail.com")).items())
+
+	def test_get_message_empty(self):
+		user1 = app.User("Bob","bob@gmail.com")
+		db.session.add(user1)
+		user2 = app.User("Amanda Georgescu","georgescu.amanda@gmail.com")
+		db.session.add(user2)
+		db.session.commit()
+		response = self.client.get('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data), [])
+
+	def test_get_message_successful_then_empty(self):
+		user1 = app.User("Bob","bob@gmail.com")
+		db.session.add(user1)
+		user2 = app.User("Amanda Georgescu","georgescu.amanda@gmail.com")
+		db.session.add(user2)
+		message = app.Message("georgescu.amanda@gmail.com","bob@gmail.com","Hey Bob")
+		db.session.add(message)
+		db.session.commit()
+		response = self.client.get('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data), [{"name":"Amanda Georgescu","text":"Hey Bob"}])
+		response = self.client.get('/messages', data='{"sender":"georgescu.amanda@gmail.com","receiver":"bob@gmail.com"}', content_type='application/json')
+		self.assertEqual(json.loads(response.data), [])
+
 
 if __name__ == '__main__':
     unittest.main()
